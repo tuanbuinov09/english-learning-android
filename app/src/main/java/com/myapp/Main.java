ec66f44f023418d47075424062719a36ed5dccc4
Tuan EnderMan <t01684599820@gmail.com>
2022-03-06 10:43:30 +0700

    update ui

--- app/src/main/java/com/myapp/Main.java
+++ app/src/main/java/com/myapp/Main.java
@@ -3,12 +3,21 @@ package com.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Main extends AppCompatActivity {
-
+    EditText searchInput = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_linearlayout);
+
+        searchInput = findViewById(R.id.searchInput);
+    }
+
+    public void search(View view){
+        Toast.makeText(this,"bạn vừa tìm: " + searchInput.getText().toString().trim(),Toast.LENGTH_LONG).show();
     }
 }
