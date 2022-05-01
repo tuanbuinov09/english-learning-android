96b75ebdc1196f8aacf7a9e44c7fd19c4b6d1627
Tuan EnderMan <t01684599820@gmail.com>
2022-05-01 18:43:29 +0700

    update

--- app/src/main/java/com/myapp/SettingsActivity.java
+++ app/src/main/java/com/myapp/SettingsActivity.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class Settings extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
