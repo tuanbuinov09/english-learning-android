739a113766834748e43eabe0ad4d9cf0e8acf0ea
Lưu Tiến Phát <luutienphat@gmail.com>
2022-05-08 23:43:37 +0700

    Add the TranslationHistoryDao to handle the CRUD methods to table TranslationHistory in db

---
+++ app/src/main/java/com/myapp/sqlite/dao/Dao.java
@@ -0,0 +1,20 @@
+package com.myapp.sqlite.dao;
+
+import java.util.List;
+
+public interface Dao<T> {
+
+    void fillData();
+
+    boolean insertOne(T t);
+
+    boolean updateOne(T t);
+
+    boolean deleteOne(T t);
+
+    List<T> getAll();
+
+    T getOne(String id);
+
+    List<T> search(String keyword);
+}
---
+++ app/src/main/java/com/myapp/sqlite/dao/TranslationHistoryDao.java
@@ -0,0 +1,152 @@
+package com.myapp.sqlite.dao;
+
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.myapp.model.TranslationHistory;
+import com.myapp.sqlite.DatabaseHelper;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TranslationHistoryDao implements Dao<TranslationHistory> {
+    private SQLiteDatabase db;
+    private final SQLiteOpenHelper dbHelper;
+
+    public TranslationHistoryDao(DatabaseHelper dbHelper) {
+        this.dbHelper = dbHelper;
+    }
+
+    @Override
+    public void fillData() {
+    }
+
+    @Override
+    public boolean insertOne(TranslationHistory translationHistory) {
+        boolean result = true;
+        db = dbHelper.getWritableDatabase();
+
+        db.beginTransaction();
+        try {
+            ContentValues cv = new ContentValues();
+            cv.put(DatabaseHelper.TABLE_TRANSLATION_HISTORY_DATE, translationHistory.getDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+            cv.put(DatabaseHelper.TABLE_TRANSLATION_HISTORY_ORIGINAL_TEXT, translationHistory.getOriginalText());
+            cv.put(DatabaseHelper.TABLE_TRANSLATION_HISTORY_ORIGINAL_TEXT, translationHistory.getOriginalText());
+
+            db.insert(DatabaseHelper.TABLE_TRANSLATION_HISTORY, null, cv);
+
+            db.setTransactionSuccessful();
+        } catch (Exception ex) {
+            result = false;
+            ex.printStackTrace();
+        } finally {
+            db.endTransaction();
+        }
+        db.close();
+
+        return result;
+    }
+
+    @Override
+    public boolean updateOne(TranslationHistory translationHistory) {
+        boolean result = true;
+        db = dbHelper.getWritableDatabase();
+
+        db.beginTransaction();
+        try {
+            ContentValues cv = new ContentValues();
+            cv.put(DatabaseHelper.TABLE_TRANSLATION_HISTORY_DATE, translationHistory.getDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+            cv.put(DatabaseHelper.TABLE_TRANSLATION_HISTORY_ORIGINAL_TEXT, translationHistory.getOriginalText());
+            cv.put(DatabaseHelper.TABLE_TRANSLATION_HISTORY_ORIGINAL_TEXT, translationHistory.getOriginalText());
+
+            db.update(DatabaseHelper.TABLE_TRANSLATION_HISTORY, cv, DatabaseHelper.TABLE_TRANSLATION_HISTORY_ID + " = ?", new String[]{String.valueOf(translationHistory.getId())});
+            db.setTransactionSuccessful();
+        } catch (Exception ex) {
+            result = false;
+            ex.printStackTrace();
+        } finally {
+            db.endTransaction();
+        }
+        db.close();
+
+        return result;
+    }
+
+    @Override
+    public boolean deleteOne(TranslationHistory translationHistory) {
+        boolean result = true;
+        db = dbHelper.getWritableDatabase();
+
+        db.beginTransaction();
+        try {
+            db.delete(DatabaseHelper.TABLE_TRANSLATION_HISTORY, DatabaseHelper.TABLE_TRANSLATION_HISTORY_ID + " = ?", new String[]{String.valueOf(translationHistory.getId())});
+            db.setTransactionSuccessful();
+        } catch (Exception ex) {
+            result = false;
+            ex.printStackTrace();
+        } finally {
+            db.endTransaction();
+        }
+        db.close();
+
+        return result;
+    }
+
+    @Override
+    public List<TranslationHistory> getAll() {
+        List<TranslationHistory> receipts = new ArrayList<>();
+        db = dbHelper.getReadableDatabase();
+
+        String queryString = "SELECT * FROM " + DatabaseHelper.TABLE_TRANSLATION_HISTORY;
+        Cursor cursor = db.rawQuery(queryString, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                int id = cursor.getInt(0);
+                String originalText = cursor.getString(1);
+                String translatedText = cursor.getString(2);
+                String date = cursor.getString(4);
+
+                LocalDate localDate = LocalDate.parse(date, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+
+                TranslationHistory translationHistory = new TranslationHistory(id, originalText, translatedText, localDate);
+                receipts.add(translationHistory);
+
+            } while (cursor.moveToNext());
+        }
+
+        cursor.close();
+        db.close();
+        return receipts;
+    }
+
+    @Override
+    public TranslationHistory getOne(String id) {
+        TranslationHistory translationHistory = null;
+        db = dbHelper.getReadableDatabase();
+        Cursor cursor = db.query(DatabaseHelper.TABLE_TRANSLATION_HISTORY, null, DatabaseHelper.TABLE_TRANSLATION_HISTORY_ID + " = ?", new String[]{String.valueOf(id)}, null, null, null);
+
+        if (cursor.moveToFirst()) {
+            int translationHistoryId = cursor.getInt(0);
+            String originalText = cursor.getString(1);
+            String translatedText = cursor.getString(2);
+            String date = cursor.getString(4);
+
+            LocalDate localDate = LocalDate.parse(date, DateTimeFormatter.ISO_DATE);
+
+            translationHistory = new TranslationHistory(translationHistoryId, originalText, translatedText, localDate);
+        }
+        return translationHistory;
+    }
+
+    @Override
+    @Deprecated
+    public List<TranslationHistory> search(String keyword) {
+        return null;
+    }
+}
