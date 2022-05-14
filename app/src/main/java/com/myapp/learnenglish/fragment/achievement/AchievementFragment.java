9a0f842ee7925890e0349ae056f91a97bbd0808a
quangtruong29 <nguyenquangtruonghktcute@gmail.com>
2022-05-15 00:33:30 +0700

    merge

--- app/src/main/java/com/myapp/learnenglish/fragment/achievement/AchievementFragment.java
+++ app/src/main/java/com/myapp/learnenglish/fragment/achievement/AchievementFragment.java
@@ -1,26 +1,14 @@
 package com.myapp.learnenglish.fragment.achievement;
 
 import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.myapp.R;
-import com.myapp.learnenglish.fragment.achievement.adapter.RVAchievementAdapter;
-import com.myapp.learnenglish.fragment.achievement.model.Achievement;
-import com.myapp.learnenglish.fragment.achievement.model.UserAchievement;
-
-import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -73,67 +61,6 @@ public class AchievementFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_achievement, container, false);
-
-        loadData(view);
-
-        return view;
-    }
-
-    private void loadData(View view) {
-        FirebaseDatabase.getInstance().getReference("Achievements").orderByKey()
-                .addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        ArrayList<Achievement> achievements = new ArrayList<>();
-                        for (DataSnapshot achievement : snapshot.getChildren()) {
-                            achievements.add(new Achievement(achievement.child("name").getValue().toString(),
-                                    achievement.child("description").getValue().toString(),
-                                    Integer.parseInt(achievement.child("requiredQuantity").getValue().toString()),
-                                    achievement.child("path").getValue().toString()));
-                        }
-
-                        FirebaseDatabase.getInstance().getReference("Topics").orderByKey()
-                            .addValueEventListener(new ValueEventListener() {
-                                @Override
-                                public void onDataChange(@NonNull DataSnapshot snapshot) {
-                                    // structure: each topic has a list of exercises, each exercise has a list of questions
-                                    int score = 0;
-                                    int learned = 0;
-                                    String currentUserId = FirebaseAuth.getInstance().getUid();
-                                    for (DataSnapshot topicsNode : snapshot.getChildren()) {
-                                        for (DataSnapshot exercisesNode : topicsNode.child("Exercises").getChildren()) {
-                                            if (exercisesNode.hasChild("Scores")) {
-                                                if (exercisesNode.child("Scores").hasChild(currentUserId)) {
-                                                    learned++;
-                                                    score += Integer.parseInt(exercisesNode.child("Scores").child(currentUserId).getValue().toString());
-                                                }
-                                            }
-                                        }
-                                    }
-                                    UserAchievement userAchievement = new UserAchievement(learned, score);
-                                    System.out.println(score + " " + learned);
-                                    initRecyclerView(view, achievements, userAchievement);
-                                }
-
-                                @Override
-                                public void onCancelled(@NonNull DatabaseError error) {
-
-                                }
-                            });
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-
-                    }
-                });
-    }
-
-    private void initRecyclerView(View view, ArrayList<Achievement> achievements, UserAchievement userAchievement) {
-        RecyclerView recyclerView = view.findViewById(R.id.rvAchievements);
-        RVAchievementAdapter adapter = new RVAchievementAdapter(getContext(), achievements, userAchievement);
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        return inflater.inflate(R.layout.fragment_achievement, container, false);
     }
 }
