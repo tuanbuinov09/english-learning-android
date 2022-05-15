e61caec78228cd24bb6bdadeac79194814725471
Thanh Tung <thanhtung28400@gmail.com>
2022-05-15 13:12:19 +0700

    fix: restore AchievementFragment

--- app/src/main/java/com/myapp/learnenglish/fragment/profile/ProfileFragment.java
+++ app/src/main/java/com/myapp/learnenglish/fragment/profile/ProfileFragment.java
@@ -1,12 +1,11 @@
 package com.myapp.learnenglish.fragment.profile;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 import com.myapp.R;
 
@@ -20,7 +19,7 @@ public class ProfileFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_PARAM2 = "param2";:
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -60,7 +59,6 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 }
