bac0dbfb8876054329f9bd02c438a0d22f4362ff
quangtruong29 <nguyenquangtruonghktcute@gmail.com>
2022-05-17 15:33:28 +0700

    add point for Tung ML

--- app/src/main/java/com/myapp/learnenglish/fragment/home/TestResultActivity.java
+++ app/src/main/java/com/myapp/learnenglish/fragment/home/TestResultActivity.java
@@ -3,6 +3,8 @@ package com.myapp.learnenglish.fragment.home;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,24 +16,43 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.myapp.Database;
 import com.myapp.R;
+import com.myapp.User;
+import com.myapp.dtbassethelper.DatabaseAccess;
 
 public class TestResultActivity extends AppCompatActivity {
     private TextView tvObtainedStars, tvInTotal;
     private Button btnContinue;
-
+    DatabaseAccess DB;
+    User user;
+    int score=0;
+    final  String DATABASE_NAME = "tudien.db";
+    SQLiteDatabase database;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(com.myapp.R.layout.activity_test_result);
-
+        DB = DatabaseAccess.getInstance(getApplicationContext());
         setControl();
         setEvent();
-
+        LayUser();
         tvInTotal.setText(getIntent().getExtras().get("total").toString());
         tvObtainedStars.setText(getIntent().getExtras().get("obtainedStars").toString());
+        DB.capnhatdiem(DB.iduser,user.getPoint(), (Integer) getIntent().getExtras().get("total"));
     }
-
+    public void LayUser()
+    {
+        database = Database.initDatabase(TestResultActivity.this, DATABASE_NAME);
+        Cursor cursor = database.rawQuery("SELECT * FROM User WHERE ID_User = ?",new String[]{String.valueOf(DB.iduser)});
+        cursor.moveToNext();
+        String Iduser = cursor.getString(0);
+        String HoTen = cursor.getString(1);
+        int Point = cursor.getInt(2);
+        String Email = cursor.getString(3);
+        String SDT = cursor.getString(4);
+        user = new User(Iduser,HoTen,Point,Email,SDT);
+    }
     private void setEvent() {
         btnContinue.setOnClickListener(new View.OnClickListener() {
             @Override
